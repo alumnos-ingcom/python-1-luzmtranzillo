89dd877e915dd715753eb3d08ead1984bd39986d
Luz <luzmtranzillo@gmail.com>
2022-06-01 01:27:18 -0300

    Correccion de errores en test_ejercicio1.py

--- tests/test_ejercicio1.py
+++ tests/test_ejercicio1.py
@@ -8,22 +8,22 @@ Test de la funciones convertir_a_centigrados y convertir_a_fahrenheit, perteneci
 """
 import pytest
 
-from src.ejercicio1 import convertir_a_centigrados, convertir_a_centigrados
+from src.ejercicio1 import convertir_a_centigrados, convertir_a_fahrenheit
 
 def test_convertir_a_centigrados():
     """
     Esta función verifica que el numero ingresado y el resultado de la conversion de la funcion convertir_a_centigrados sea de tipo flotante.
     """
-    fahrenheit=32
+    fahrenheit=32.0
     resultado=convertir_a_centigrados(fahrenheit)
-    assert isinstance(centigrados,float),"El numero ingresado debe se un numero de tipo flotante"
+    assert isinstance(fahrenheit,float),"El numero ingresado debe se un numero de tipo flotante"
     assert isinstance(resultado,float),"El resultado debe se un numero de tipo flotante"
 
 def test_convertir_a_fahrenheit():
     """
     Esta función verifica que el numero ingresado y el resultado de la conversion de la funcion convertir_a_fahrenit sea de tipo flotante. 
     """
-    centigrados=32
+    centigrados=32.0
     resultado=convertir_a_fahrenheit(centigrados)
     assert isinstance(centigrados,float),"El numero ingresado debe se un numero de tipo flotante"
     assert isinstance(resultado,float),"El resultado debe se un numero de tipo flotante"
