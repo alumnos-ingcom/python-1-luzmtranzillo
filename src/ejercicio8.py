4fe91a1f687e6be2dbc635f0cf83f3ce214a24fd
Luz <luzmtranzillo@gmail.com>
2022-06-02 23:42:27 -0300

    Subida del ejercicio8.py a la carpeta src

---
+++ src/ejercicio8.py
@@ -0,0 +1,34 @@
+################
+# Nombre - @luzmtranzillo
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+Escribir una función que indique con True si un numero indicado es Primo.
+"""
+
+
+def es_primo(numero):
+    if numero> 1:
+        for n in range(2,numero):
+            if (numero % n) == 0:
+                return False
+        return True
+    else:
+        return False
+        
+def principal():
+    """
+    Descripción del ejercicio
+    Precondiciones: Ingreso de un número de tipo entero.
+    Postcondiciones: Salida de un mensaje declarando si el número ingresado es primo o no de acuerdo al retorno de la función es_primo().
+    """
+    numero=int(input('Ingrese un número '))
+    tipo=es_primo(numero)
+    if tipo:
+        print('El numero ingresado es un número primo')
+    else:
+        print('El número ingresado no es un número primo')
+
+if __name__ == "__main__":
+    principal()
