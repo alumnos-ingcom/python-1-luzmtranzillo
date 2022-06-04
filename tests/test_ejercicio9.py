ccbd2ac56e284641a8c37083f19757eae491700c
Luz <luzmtranzillo@gmail.com>
2022-06-04 20:41:11 -0300

    Subida del testeo perteneciente al ejercicio9.py

---
+++ tests/test_ejercicio9.py
@@ -0,0 +1,22 @@
+################
+# Nombre - @luzmtranzillo
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+Función que testea la funcion factores_primos(numero) del ejercicio9.py
+"""
+import pytest
+
+from src.ejercicio1 import factores_primos
+
+def test_factores_primos():
+    """
+    Función que testea que el tipo de entra y salida sean correctas para la funcion factores_primos(numero), y que su resultado sea el correcto. 
+    """
+    numero=10
+    resultado=factores_primos(numero)
+    assert isinstance(numero, int), 'Debe ingresar un número de tipo int.'
+    assert isinstance(resultado, tupla), 'La función debe retornar una tupla de números enteros.'
+    assert resultado==(2, 5), 'La tupla se ha generado erroneamente.'
+    assert numero==>1, 'Números menores a 1 no tienen factores primos.'
