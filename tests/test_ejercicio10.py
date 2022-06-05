e3ccf1b24ffc00ac367c1c5c01452ab473f353d1
Luz <luzmtranzillo@gmail.com>
2022-06-05 02:38:14 -0300

    Modificaciones en el test perteneciente al ejercicio10.py para añadir casos de prueba

--- tests/test_ejercicio10.py
+++ tests/test_ejercicio10.py
@@ -11,12 +11,22 @@ import pytest
 
 from src.ejercicio10 import es_palindromo
 
-def test_es_palindromo():
+def test_es_palindromo_true():
     """
-    Test que verifica que el tipo de entrada y salida de la funcion es_palindromo(texto) sea correcta. Y que el retorno tambíen lo sea. 
+    Test que verifica que el tipo de entrada y salida de la funcion es_palindromo(texto) sea correcta. Y que el retorno tambíen lo sea para el caso True. 
     """
     texto='neuquen'
     resultado=es_palindromo(texto)
     assert isinstance(texto, str), 'Debe ingresar una palabra o frase de tipo string.'
     assert isinstance(resultado, str), 'La función debe retornar una palabra o frase de tipo string.'
     assert resultado=='es un palindromo', 'La transformación del texto se ha realizado de manera incorrecta. El texto debiera ser palindromo.'
+       
+def test_es_palindromo_false():
+    """
+    Test que verifica que el tipo de entrada y salida de la funcion es_palindromo(texto) sea correcta. Y que el retorno tambíen lo sea para el caso False. 
+    """
+    texto='reparacion'
+    resultado=es_palindromo(texto)
+    assert isinstance(texto, str), 'Debe ingresar una palabra o frase de tipo string.'
+    assert isinstance(resultado, str), 'La función debe retornar una palabra o frase de tipo string.'
+    assert resultado=='no es un palindromo', 'La transformación del texto se ha realizado de manera incorrecta. El texto no debiera ser palindromo.'
