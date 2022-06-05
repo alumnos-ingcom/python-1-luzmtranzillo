72bae453ada5a2b64236960cb58c64d1dd01e528
Luz <luzmtranzillo@gmail.com>
2022-06-05 02:25:17 -0300

    Modificaciones en el test perteneciente al ejercicio1.py para añadir casos de prueba

--- tests/test_ejercicio1.py
+++ tests/test_ejercicio1.py
@@ -10,22 +10,42 @@ import pytest
 
 from src.ejercicio1 import convertir_a_centigrados, convertir_a_fahrenheit
 
-def test_convertir_a_centigrados():
+def test_convertir_a_centigrados_positivo():
     """
-    Esta función verifica que el numero ingresado y el resultado de la conversion de la funcion convertir_a_centigrados sea de tipo flotante.
+    Esta función verifica que el número ingresado y el resultado de la conversion de la funcion convertir_a_centigrados sea de tipo flotante para grados positivos.
     """
     fahrenheit=32.0
     resultado=convertir_a_centigrados(fahrenheit)
     assert isinstance(fahrenheit,float),"El numero ingresado debe se un numero de tipo flotante"
     assert isinstance(resultado,float),"El resultado debe se un numero de tipo flotante"
     assert resultado==0.0, 'El resultado debe ser un número de tipo float igual a 0.0'
-
-def test_convertir_a_fahrenheit():
+    
+def test_convertir_a_centigrados_negativo():
     """
-    Esta función verifica que el numero ingresado y el resultado de la conversion de la funcion convertir_a_fahrenit sea de tipo flotante. 
+    Esta función verifica que el número ingresado y el resultado de la conversion de la funcion convertir_a_centigrados sea de tipo flotante para grados negativos.
+    """
+    fahrenheit=-32.0
+    resultado=convertir_a_centigrados(fahrenheit)
+    assert isinstance(fahrenheit,float),"El numero ingresado debe se un numero de tipo flotante"
+    assert isinstance(resultado,float),"El resultado debe se un numero de tipo flotante"
+    assert resultado==-35.55555555555556, 'El resultado debe ser un número de tipo float igual a -35.55555555555556'
+
+def test_convertir_a_fahrenheit_positivo():
+    """
+    Esta función verifica que el número ingresado y el resultado de la conversion de la funcion convertir_a_fahrenit sea de tipo flotante para grados positivos. 
     """
     centigrados=32.0
     resultado=convertir_a_fahrenheit(centigrados)
     assert isinstance(centigrados,float),"El numero ingresado debe se un numero de tipo flotante"
     assert isinstance(resultado,float),"El resultado debe se un numero de tipo flotante"
     assert resultado==89.6, 'El resultado debe ser un número de tipo float igual a 89.6'
+    
+def test_convertir_a_fahrenheit_negativo():
+    """
+    Esta función verifica que el número ingresado y el resultado de la conversion de la funcion convertir_a_fahrenit sea de tipo flotante para grados negativos. 
+    """
+    centigrados=-32.0
+    resultado=convertir_a_fahrenheit(centigrados)
+    assert isinstance(centigrados,float),"El numero ingresado debe se un numero de tipo flotante"
+    assert isinstance(resultado,float),"El resultado debe se un numero de tipo flotante"
+    assert resultado==-25.6, 'El resultado debe ser un número de tipo float igual a -25.6'
