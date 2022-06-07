a47aa886cd51fc7a60cde20d1221ad40f83c981b
Luz <luzmtranzillo@gmail.com>
2022-06-07 16:24:17 -0300

    Cambios en los assert que usaban la funcion signo() por cambios en su codigo

--- tests/test_ejercicio7.py
+++ tests/test_ejercicio7.py
@@ -27,9 +27,9 @@ def test_sexadecimal_a_decimal():
     assert isinstance(minutos, float), 'Debe ingresar un valor de tipo float.'
     assert isinstance(segundos, float), 'Debe ingresar un valor de tipo float.'
     assert isinstance(resultado, float), 'Debe retornar un valor de tipo float.'
-    assert signo(horas)=='positivo', 'Debe ingresar un valor positivo.'
-    assert signo(minutos)=='positivo', 'Debe ingresar un valor positivo.'
-    assert signo(segundos)=='positivo', 'Debe ingresar un valor positivo.'
+    assert signo(horas)==1, 'Debe ingresar un valor positivo.'
+    assert signo(minutos)==1, 'Debe ingresar un valor positivo.'
+    assert signo(segundos)==1, 'Debe ingresar un valor positivo.'
     assert resultado==73220.0 ,'La conversión se ha realizado de manera erronea.'
   
     
@@ -41,5 +41,5 @@ def test_decimal_a_sexadecimal():
     resultado=decimal_a_sexadecimal(numero)
     assert isinstance(numero, float), 'Debe ingresar un número de tipo float.'
     assert isinstance(resultado, tuple), 'Debe retornar una tupla con la conversión viceversa de sexadecimal_a_decimal().'
-    assert signo(numero)=='positivo', 'Debe ingresar un valor para número positivo.'
+    assert signo(numero)==1, 'Debe ingresar un valor para número positivo.'
     assert resultado==(20, 20, 20), 'La tupla generada es incorrecta.'
